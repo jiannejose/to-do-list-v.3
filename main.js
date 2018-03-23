c42bb9d46e7c80ad749b3de8a9d7d30c6b9fc1ec
jiannejose <annejizrelle@gmail.com>
2018-03-23 21:54:37 +0800

    Add Task

--- main.js
+++ main.js
@@ -3,19 +3,75 @@ let allTasks = [];
 let incompleteTasks = document.getElementById('incompleteTasks');
 let completedTasks = document.getElementById('completedTasks');
 
+let submitForm = document.getElementById('addTask');
 let inputTask = document.getElementById('taskInput');
-let addTaskButton = document.getElementById('addTask');
+let monthSelect = document.querySelector('[name="months"]');
+let daySelect = document.querySelector('[name="day"]');
+let yearSelect = document.querySelector('[name="year"]');
 
 inputTask.focus();
 
-addTaskButton.addEventListener('click', addTask);
+submitForm.addEventListener('submit', addTask);
 
 function addTask(e) {
   e.preventDefault();
+  
+  let taskName = inputTask.value;
+  let month = monthSelect[monthSelect.selectedIndex].value;
+  let day = daySelect[daySelect.selectedIndex].value;
+  let year = yearSelect[yearSelect.selectedIndex].value;
 
-  let parentElement = this.parentElement;
+  let index = allTasks.length;
 
-  let selectMonth = parentElement.querySelector('[name="month"]');
-  let selectDay = parentElement.querySelector('[name="day"]');
-  let selectYear = parentElement.querySelector('[name="year"]');
+  allTasks.push({
+    'id': index,
+    'name': taskName,
+    'due_date': new Date(`${year}-${month}-${day}`),
+    'is_completed': false,
+  });
+
+  inputTask.value = '';
+  renderIncompleteTask(allTasks[index]);
+}
+
+function formatDate(date) {
+  var monthNames = [
+    "January", "February", "March",
+    "April", "May", "June", "July",
+    "August", "September", "October",
+    "November", "December"
+  ];
+
+  let day = date.getDate();
+  let monthIndex = date.getMonth();
+  let year = date.getFullYear();
+
+  return `${monthNames[monthIndex]}, ${day}, ${year}`;
+}
+
+function renderIncompleteTask(task) {
+  let newElement = document.createElement('li');
+
+  newElement.classList.add('js-task-item');
+  newElement.setAttribute('data-index', task.id);
+
+  newElement.innerHTML = incompleteTasksTemplate(task);
+  incompleteTasks.appendChild(newElement);
+}
+
+function incompleteTasksTemplate(task) {
+
+  incompleteTasks.innerHTML += `
+    <li>
+        <h5>${task.name}</h5>
+
+        <h5>${formatDate(task.due_date)}</h5>
+
+        <div class="actions">
+            <button type="submit" class="edit">Edit</button>
+            <button type="submit" class="delete">Delete</button>
+            <button type="submit" class="done">Done</button>
+        </div>
+    </li>
+  `;
 }
